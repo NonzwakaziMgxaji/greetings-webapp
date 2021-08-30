1f6a07b3c557f44f855df0acd144f8a703d9df96
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2021-08-30 15:16:06 +0200

    trying to fix failing tests

--- test/databaseTest.js
+++ test/databaseTest.js
@@ -3,7 +3,6 @@ const greetFactory = require('../greeting');
 const pg = require("pg");
 const Pool = pg.Pool;
 
-// we are using a special test database for the tests
 const connectionString = process.env.DATABASE_URL || 'postgresql://nzwakie:Bokang2851!@localhost:5432/names_greeted';
 
 const pool = new Pool({
@@ -13,7 +12,6 @@ const pool = new Pool({
 describe('The greetings-webapp database', function () {
 
     beforeEach(async function () {
-        // clean the tables before each test run
         await pool.query("delete from users;");
     });
 
